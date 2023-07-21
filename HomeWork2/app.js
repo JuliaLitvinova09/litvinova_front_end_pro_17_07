27e128cd70fac3a7a941543bd5567993259f8a57
Julia Litvonova <julia.lit1978@gmail.com>
2023-07-21 11:06:47 +0300

    Home work 2. Second commit.

--- HomeWork2/app.js
+++ HomeWork2/app.js
@@ -6,9 +6,10 @@ alert("Hello, " + (userName || "Stranger") + "! How are you?");
 
 // 2. Створити скрипт для складання, віднімання, множення та поділу двох чисел та виведення результатів
 
-let a = 100,
-  b = 20,
-  c = 60;
+let a, b, c;
+
+a = Number(prompt("Enter the first number for mathematical operations:", 0));
+b = Number(prompt("Enter the second number for mathematical operations:", 0));
 
 console.log(a + b, "(a+b)");
 console.log(a - b, "(a-b)");
@@ -16,21 +17,22 @@ console.log(a * b, "(a*b)");
 console.log(a / b, "(a/b)");
 
 // 3. Створити скрипт, який отримує два значення і виводить true, якщо значення рівні, false - якщо ні
-a = prompt("Enter the first number:");
-b = prompt("Enter the second number:");
+a = prompt("Enter the first value for comparison:");
+b = prompt("Enter the second value for comparison:");
 
 console.log(a === b, "same (true) or not (false)");
 
 // 4. Визначити середнє арифметичне трьох чисел
-a = 100;
-b = 20;
-c = 60;
+a = Number(prompt("Enter the first number for average:", 0));
+b = Number(prompt("Enter the second number for average:", 0));
+c = Number(prompt("Enter the third number for average:", 0));
+
 console.log((a + b + c) / 3, "average");
 
 // 5. Розкласти по цифрах п'ятизначне число і вивести у вихідному порядку через пробіл
 //  (використовувати оператор “розподіл по модулю” (%)
 
-a = 12345;
+a = Number(prompt("Enter the number you wanted to decompose:", 0));
 
 console.log(
   `${(a - (a % 10000)) / 10000} ${((a - (a % 1000)) / 1000) % 10} ${
